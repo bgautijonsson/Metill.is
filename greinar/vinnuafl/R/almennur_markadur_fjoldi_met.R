cee3a5acbab3b5b754828de05bd1a71a7211119d
bgautijonsson <bgautijonsson@gmail.com>
2024-05-10 10:30:15 +0000

    Bæta veltufé sem hlutfall af nettóskuldum í sveitarfélagagögn

--- greinar/vinnuafl/R/almennur_markadur_fjoldi_met.R
+++ greinar/vinnuafl/R/almennur_markadur_fjoldi_met.R
@@ -8,14 +8,44 @@ theme_set(theme_metill(type = "standalone"))
 
 d <- vroom::vroom("greinar/vinnuafl/data/vinnuafl.csv")
 
-p <- d |> 
+prev_record <- d |> 
+  filter(bakgrunnur == "Alls",
+         rekstrarform != "Alls starfandi") |> 
+  count(dags, tegund, wt = starfandi) |> 
+  filter(
+    tegund == "Annad",
+    dags == clock::date_build(2018, 8)
+  ) |> 
+  pull(n)
+
+min_2024 <- d |> 
+  filter(bakgrunnur == "Alls",
+         rekstrarform != "Alls starfandi") |> 
+  count(dags, tegund, wt = starfandi) |> 
+  filter(
+    tegund == "Annad",
+    dags == clock::date_build(2024, 1)
+  ) |> 
+  pull(n)
+
+d |> 
+  filter(bakgrunnur == "Alls",
+         rekstrarform != "Alls starfandi") |> 
+  count(dags, tegund, wt = starfandi) |> 
+  filter(
+    tegund == "Annad",
+    dags == clock::date_build(2024, 3)
+  ) |> 
+  pull(n)
+
+ p <- d |> 
   filter(bakgrunnur == "Alls",
          rekstrarform != "Alls starfandi") |> 
   count(dags, tegund, wt = starfandi) |> 
   filter(tegund == "Annad") |>
   ggplot(aes(dags, n)) +
   geom_texthline(
-    yintercept = 152276, 
+    yintercept = prev_record, 
     lty = 2,
     label = "Starfandi í janúar 2024 voru bara 1% færri en í ágúst 2018",
     hjust = 0.05,
@@ -84,78 +114,6 @@ ggsave(
 
 
 
-p <- d |> 
-  filter(bakgrunnur == "Alls",
-         rekstrarform != "Alls starfandi") |> 
-  count(dags, tegund, wt = starfandi) |> 
-  filter(tegund == "Annad") |>
-  ggplot(aes(dags, n)) +
-  geom_texthline(
-    yintercept = 152276, 
-    lty = 2,
-    label = "Starfandi í janúar 2024 voru bara 1% færri en í ágúst 2018",
-    hjust = 0.05,
-    size = 3,
-    alpha = 0.5
-  ) +
-  geom_line() +
-  geom_point(
-    data = ~filter(.x, month(dags) == 1),
-    col = "#377eb8",
-    size = 2
-  ) +
-  geom_point(
-    data = ~filter(.x, month(dags) == 8),
-    col = "#e41a1c",
-    size = 2
-  ) +
-  geom_mark_hull(
-    aes(
-      label = "Janúar",
-      filter = (dags == clock::date_build(2021)),
-      description = "En minnstur í janúar hvers árs"
-    ),
-    col = "#377eb8",
-    label.fill = NA,
-    con.arrow = arrow(type = "closed", length = unit(0.25, "cm"))
-  ) +
-  geom_mark_hull(
-    aes(
-      label = "Ágúst",
-      filter = (dags == clock::date_build(2013, 7)),
-      description = "Venjulega er fjöldi starfandi mestur í ágúst hvers árs"
-    ),
-    col = "#e41a1c",
-    label.fill = NA,
-    con.arrow = arrow(type = "closed", length = unit(0.25, "cm"))
-  ) +
-  scale_x_date(
-    breaks = clock::date_build(2008:2024),
-    labels = label_date_short(),
-    guide = guide_axis_truncated()
-  ) +
-  scale_y_continuous(
-    labels = label_number(),
-    breaks = breaks_extended(8),
-    guide = guide_axis_truncated()
-  ) +
-  labs(
-    x = NULL,
-    y = NULL,
-    title = "Fjöldi starfandi á almennum vinnumarkaði er í methæðum",
-    subtitle = "Að öllu óbreyttu verður minnsti fjöldi starfandi árið 2024 jafnhár og metfjöldinn fyrir COVID-19 faraldur",
-    caption = str_c(
-      "Mynd eftir @bggjonsson hjá metill.is byggð á gögnum Hagstofu um starfandi samkvæmt skrám\n",
-      "Kóði: https://github.com/bgautijonsson/Metill.is/blob/master/greinar/vinnuafl/R/almennur_markadur_fjoldi_met.R"
-    )
-  )
-
-
-ggsave(
-  plot = p,
-  filename = "greinar/vinnuafl/figures/almennur_fjoldi_methaedir.png",
-  width = 8, height = 0.621 * 8, scale = 1.3
-)
 
 
 
