d35f0a20a0788d0b55cad6b76561177cd75c9eb9
bgautijonsson <bgautijonsson@gmail.com>
2024-06-07 21:53:22 +0000

    Uppfæra vinnuafl og flottafolk

--- greinar/fasteignafjoldi/R/get_pop_data.R
+++ greinar/fasteignafjoldi/R/get_pop_data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(eurostat)
+library(arrow)
 
 pop <- get_eurostat(
   "demo_pjan",
@@ -41,7 +42,7 @@ d <- d |>
   janitor::remove_constant() 
 
 
-d |> 
+d <- d |> 
   filter(
     !age %in% c("Total", "Unknown", "Open-ended age class")
   ) |> 
@@ -61,5 +62,37 @@ d |>
     .by = c(geo, time)
   ) |> 
   mutate(year = year(time)) |> 
-  select(country = geo, year, population_adult) |>
+  select(country = geo, year, population_adult)
+
+library(hagstofa)
+
+url <- "https://px.hagstofa.is:443/pxis/api/v1/is/Ibuar/mannfjoldi/1_yfirlit/Yfirlit_mannfjolda/MAN00101.px"
+
+
+d_hg <- hg_data(url) |> 
+  filter(
+    parse_number(Aldur) > 18,
+    Kyn == "Alls",
+    parse_number(Ár) >= 2007
+  ) |> 
+  collect() |> 
+  janitor::clean_names() |> 
+  mutate_at(
+    vars(aldur, ar),
+    parse_number
+  ) |> 
+  rename(n = 4) |> 
+  janitor::remove_constant() |> 
+  summarise(
+    population_adult = sum(n),
+    .by = ar
+  ) |> 
+  rename(year = ar) |> 
+  mutate(
+    country = "Iceland"
+  )
+
+d |> 
+  filter(country != "Iceland") |> 
+  bind_rows(d_hg) |> 
   write_parquet("greinar/fasteignafjoldi/data/data_pop.parquet")
